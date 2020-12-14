eaa38484930c351e10cad049a31de8c9e010fd6c
Sridharan S <aurosridhar@gmail.com>
2020-12-14 05:43:14 +0530

    jna added

---
+++ src/DIMap1.pas
@@ -0,0 +1,124 @@
+(*
+Copyright (C) 2020, Sridharan S
+
+This file is part of Inscript Keyboard.
+
+Inscript Keyboard is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Inscript Keyboard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License version 3 
+along with Inscript Keyboard.  If not, see <http://www.gnu.org/licenses/>.
+*)
+{
+Tamil Unicode Standard Font
+Inscript Layout
+}
+unit DIMap1;
+
+interface
+
+uses
+  SysUtils;
+
+const
+// TAU
+//UTkee: array [0..79] of ansiístring =(
+//UIkee: array [0..337] of ansistring =(
+UIkee: array [0..90] of ansistring =(
+{ Vowels}
+'d',      'e',      'f',      'r',    'g',      't',      'z',      's',    'w',   '`',
+'a',      'q', 'X', 'x',           '_',
+{ Consonants}
+'k', 'K', 'i', 'I', 'U',
+';', ':', 'p', 'P', '}',
+'''','"', '[', '{', 'C',
+'l', 'L', 'o', 'O', 'v',
+'h', 'H', 'y', 'Y', 'c',
+
+'/',      'j',      'n',      'b',      'B',      'N',      'J',      'V',
+{ Deva Nagari}
+'M', '<', 'm', 'u',
+'%', '$', '^', '&', '*',
+ '>',
+{ Numerals}
+'0',      '1',      '2',      '3',     '4',       '5',      '6',      '7',      '8',      '9',
+{ Duplicates}
+{ Symbols}
+',',     '.',       '?',
+'~',     '@',       '#',
+//'%',
+'(',      ')',      '+',      '-',
+
+'kd',
+'ke',
+'kf',
+'kr',
+// U and UU
+'kg',
+'kt',
+'k=',
+'kz',
+'ks',
+'kw',
+'k`',
+'ka',
+'kq'
+);
+
+//UTFee: array [0..159] of integer =(
+//UIFee: array [0..1351] of integer =(
+UIFee: array [0..363] of integer =(
+{ Vowels}
+  $B85,0,0,0,    $B86,0,0,0,    $B87,0,0,0,    $B88,0,0,0,    $B89,0,0,0,    $B8A,0,0,0,    $B8E,0,0,0,    $B8F,0,0,0,    $B90,0,0,0,
+  $B92,0,0,0,    $B93,0,0,0,    $0B94,0,0,0,
+  $901,0,0,0,    $B82,0,0,0,    $B83,0,0,0,
+{ Consonants}
+  $B95,0,0,0,    $B95,0,0,0,    $B95,0,0,0,    $B95,0,0,0,    $B99,0,0,0,
+  $B9A,0,0,0,    $B9A,0,0,0,    $B9C,0,0,0,    $B9C,0,0,0,    $B9E,0,0,0,
+  $B9F,0,0,0,    $B9F,0,0,0,    $B9F,0,0,0,    $B9F,0,0,0,    $BA3,0,0,0,
+  $BA4,0,0,0,    $BA4,0,0,0,    $BA4,0,0,0,    $BA4,0,0,0,    $BA8,0,0,0,
+  $BAA,0,0,0,    $BAA,0,0,0,    $BAA,0,0,0,    $BAA,0,0,0,    $BAE,0,0,0,
+
+  $BAF,0,0,0,    $BB0,0,0,0,    $BB2,0,0,0,    $BB5,0,0,0,    $BB4,0,0,0,    $BB3,0,0,0,    $BB1,0,0,0,    $BA9,0,0,0,
+{ Deva Nagari}
+  $BB6,0,0,0,    $BB7,0,0,0,    $BB8,0,0,0,    $BB9,0,0,0,
+  $B9C,$BCD,$B9E,0,    $BB0,$BCD,0,0,    $BA4,$BCD,$BB0,0,    $B95,$BCD,$BB7,0,    $BB6,$BCD,$BB0,0,
+  $BB8,$BCD,$BB0,$BC0,
+{ Numerals}
+  48,0,0,0,      49,0,0,0,      50,0,0,0,      51,0,0,0,      52,0,0,0,      53,0,0,0,      54,0,0,0,      55,0,0,0,      56,0,0,0,      57,0,0,0,
+{ Duplicates}
+{ Symbols}
+  $2C,0,0,0,     $2E,0,0,0,     $2D,0,0,0,
+  $2A,0,0,0,     $22,0,0,0,     $25,0,0,0,
+//  $BC1,0,0,0,
+  40,0,0,0,      41,0,0,0,
+  //61,0,0,0,
+  43,0,0,0,      45,0,0,0,
+
+  $BCD,0,0,0,
+  $BBE,0,0,0,
+  $BBF,0,0,0,
+  $BC0,0,0,0,
+  $BC1,0,0,0,
+  $BC2,0,0,0,
+  $BCD,$BB0,$BC1,0,
+  $BC6,0,0,0,
+  $BC7,0,0,0,
+  $BC8,0,0,0,
+  $BCA,0,0,0,
+  $BCB,0,0,0,
+  $BCC,0,0,0
+);
+
+
+
+implementation
+
+end.
---
+++ src/DIMap2.pas
@@ -0,0 +1,128 @@
+(*
+Copyright (C) 2020, Sridharan S
+
+This file is part of Inscript Keyboard.
+
+Inscript Keyboard is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Inscript Keyboard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License version 3
+along with Inscript Keyboard.  If not, see <http://www.gnu.org/licenses/>.
+*)
+{
+Devanagari Unicode Standard Font
+Inscript Layout
+}
+unit DIMap2;
+
+interface
+
+uses
+  SysUtils;
+
+const
+// Devanagari Inscript
+// 2082, 2083, 2084
+//UTkee: array [0..79] of ansiístring =(
+UDkee: array [0..90] of ansistring =(
+{ Vowels}
+'d',      'e',      'f',      'r',    'g',      't',      'z',      's',    'w',   '`',
+'a',      'q',      'X',     'x',   '_',
+{ Consonants}
+'k', 'K', 'i', 'I', 'U',
+';', ':', 'p', 'P', '}',
+'''','"', '[', '{', 'C',
+'l', 'L', 'o', 'O', 'v',
+'h', 'H', 'y', 'Y', 'c',
+'/', 'j', 'n', 'b',
+'B', 'N', 'J', 'V',
+{ Deva Nagari}
+'M', '<', 'm', 'u',
+'%', '$', '^', '&', '*',
+ '>',
+{ Numerals}
+'0',      '1',      '2',      '3',     '4',       '5',      '6',      '7',      '8',      '9',
+{ Duplicates}
+{ Symbols}
+',',     '.',       '?',
+'~',     '@',       '#',
+//'%',
+'(',      ')',      '+',      '-',
+
+'kd',
+'ke',
+'kf',
+'kr',
+// U and UU
+'kg',
+'kt',
+'k=',
+//SHORT E
+'kz',
+'ks',
+'kw',
+'k`',
+'ka',
+'kq'
+);
+
+//UTFee: array [0..159] of integer =(
+UDFee: array [0..363] of integer =(
+{ Vowels}
+  $905,0,0,0,    $906,0,0,0,    $907,0,0,0,    $908,0,0,0,    $909,0,0,0,    $90A,0,0,0,    $90E,0,0,0,    $90F,0,0,0,    $910,0,0,0,
+  $912,0,0,0,    $913,0,0,0,    $914,0,0,0,
+  $901,0,0,0,    $902,0,0,0,    $903,0,0,0,
+{ Consonants}
+  $915,0,0,0,    $916,0,0,0,    $917,0,0,0,    $918,0,0,0,    $919,0,0,0,
+  $91A,0,0,0,    $91B,0,0,0,    $91C,0,0,0,    $91D,0,0,0,    $91E,0,0,0,
+  $91F,0,0,0,    $920,0,0,0,    $921,0,0,0,    $922,0,0,0,    $923,0,0,0,
+  $924,0,0,0,    $925,0,0,0,    $926,0,0,0,    $927,0,0,0,    $928,0,0,0,
+  $92A,0,0,0,    $92B,0,0,0,    $92C,0,0,0,    $92D,0,0,0,    $92E,0,0,0,
+  $92F,0,0,0,    $930,0,0,0,    $932,0,0,0,    $935,0,0,0,
+  $934,0,0,0,    $933,0,0,0,    $931,0,0,0,    $929,0,0,0,
+{ Deva Nagari}
+  $936,0,0,0,    $937,0,0,0,    $938,0,0,0,    $939,0,0,0,
+  $91C,$94D,$91E,0,    $930,$94D,0,0,    $924,$94D,$930,0,    $915,$94D,$937,0,    $936,$94D,$930,0,
+  //$936,$94D,$930,$940,
+  $964,0,0,0,
+{ Numerals}
+  48,0,0,0,      49,0,0,0,      50,0,0,0,      51,0,0,0,      52,0,0,0,      53,0,0,0,      54,0,0,0,      55,0,0,0,      56,0,0,0,      57,0,0,0,
+{ Duplicates}
+{ Symbols}
+  $2C,0,0,0,     $2E,0,0,0,     $2D,0,0,0,
+  $2A,0,0,0,     $22,0,0,0,     $25,0,0,0,
+//  $BC1,0,0,0,
+  40,0,0,0,      41,0,0,0,
+//  61,0,0,0,
+  43,0,0,0,      45,0,0,0,
+
+
+  $94D,0,0,0,
+  $93E,0,0,0,
+  $93F,0,0,0,
+  $940,0,0,0,
+  $941,0,0,0,
+  $942,0,0,0,
+
+  $943,0,0,0,
+//SHORT E
+  $946,0,0,0,
+  $947,0,0,0,
+  $948,0,0,0,
+  $94A,0,0,0,
+  $94B,0,0,0,
+  $94C,0,0,0
+);
+
+
+
+implementation
+
+end.
